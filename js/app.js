68d2a875b52d3da3bafb426a1c8e792f0105555c
SundramAgarwal <89451119+SundramAgarwal@users.noreply.github.com>
2021-08-30 18:42:47 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$('.food-slider').slick({
+		slidesToShow: 3,
+		slidesToSroll: 1,
+		prevArrow: ".prev-btn",
+		nextArrow: ".next-btn",
+		autoplay : true,
+		responsive:[ 
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+			}
+		}
+	]
+
+	});
+
+	$('.nav-trigger').click(function(){
+		$('.site-content-wrapper').toggleClass('scaled');
+	})
+
+});
